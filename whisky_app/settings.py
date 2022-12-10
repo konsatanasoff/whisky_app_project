6310c8bd8dc7771c7f3863d6e966daa5941c09cd
Kons <Konsatanasoff@gmail.com>
2022-12-10 19:38:46 +0200

    database configuration

--- whisky_app/settings.py
+++ whisky_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'psycopg2',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'whisky_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ender3printer',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
